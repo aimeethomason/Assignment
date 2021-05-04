2211c118c7873bbfa1a28cfccdd2a21aa66c622c
aimeethomason <83536554+aimeethomason@users.noreply.github.com>
2021-05-04 19:52:56 +0100

    Add files via upload

--- Assignment.py
+++ Assignment.py
@@ -5,201 +5,226 @@ Created on Sat Apr 24 10:06:41 2021
 @author: Aimee Thomason
 """
 
-# Import relevant packages from the python library.
+# The first section of code will import the relevant packages into python as well as
+# creating variables and lists.
+
 # Import matplotlib is a comprehensive library for creating static, animated, and 
-# interactive visualizations in Python.
+# interactive visualisations in Python.
 
 import matplotlib
 
-# The following code will render matplotlib to a Tk canvas.
+# Matplotlib also has different "backends" which allows it to render its graphics in 
+# different ways. The following code will change the backend to render as associated 
+# with TkInter. This must be placed before any other matplotlib imports.
 
 matplotlib.use('TkAgg')
 
 # Import random provides access to random functions.
-# Import operator supplies functions that are equivalent to Python's operators.
 # Import matplotlib.pyplot allows data to be plotted in graphs.
-# Import matplotlib.animation to make a live animation.
+# Import matplotlib.animation allows for a live animation.
 # Import time provides access to time functions.
-# Import agent framework module to acess the agents in a random location.
-# Import csv module to enable access to csv files.
+# Import af_assignment provides access to the agents in a random location.
+# Import csv module provides access to CSV files.
 # Import tkinter provides access to the Tk GUI toolkit.
 # Import requests provides access to HTTP library, enabling HTTP requests in Python, 
 # reducing complexities behind the interface.
-# Import bs4 is a python library for pulling data out of HTML and XML files. This provides
-# idiomic ways of navigating, searching, and modifying the parse tree, saving the programmer time.
+# Import bs4 is a python library for pulling data out of HTML and XML files. This 
+# provides idiomatic ways of navigating, searching, and modifying the parse tree, 
+# saving the programmer time.
 
 import random
-import operator
 import matplotlib.pyplot
 import matplotlib.animation
-#import time
+import time
 import af_assignment
 import csv
 import tkinter
 import requests
 import bs4
 
-# The following code creates a new variable 'r' which scrapes the http data on 
-# the web, downloading and printing the y and x data.
+# The following code creates a new variable 'r' which scrapes the HTTP data on 
+# the web and downloads the y and x coordinates.
 
 r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
 content = r.text
 soup = bs4.BeautifulSoup(content, 'html.parser')
 td_ys = soup.find_all(attrs={"class" : "y"})
 td_xs = soup.find_all(attrs={"class" : "x"})
+
+# To test that the model is initiated with data from the web, the y and x 
+# coordinates from the HTTP data are printed.
+
 print(td_ys)
 print(td_xs)
 
-# The following code will create a new variable 'f' which will open the CSV 'in.txt' file.
+# Here, a new variable 'f' is created which will open the CSV 'in.txt' file.
 
 f = open('in.txt', newline=(''))
 
-# A new variable called 'reader' is created which allows the CSV file to be read within
-# python.
+# A new variable called 'reader' is also created to allow for the CSV file to be 
+# read within python.
 
 reader = csv.reader(f, quoting = csv.QUOTE_NONNUMERIC)    
 
-# Create an empty list named Environment.
+# To create an environment that allows agents to later interact with, an empty 
+# list called environment is created.
 
 environment = []
 
-# The following code will create a new list named rowlist, for each row in the read
-# csv file. The values in each row of the CSV file can then be appended to the rowlist.
-# The rowlist can then be appended to the environment. This will allow each row to be a 
-# new list in the environment, with each row filled with values.
-
+# The following code uses a for-loop that will allow each row to be a new list in
+# the environment, with each row containing values.
 for row in reader:
+    # Here a new list called rowlist is created.
     rowlist = []
-    for value in row:
-        rowlist.append(value)
+    for value in row:       
+        # The values in each row of the CSV file are appended to the rowlist.
+        rowlist.append(value)        
+    # The rowlist is then appended to the environment.
     environment.append(rowlist)
     
-# Test that the environment has been appended to the model.
+# To test that the environment has been appended to the model, the environment can
+# be printed.
     
 print(environment)
-   
 
-# Test that the self.x and self.y co-ordinates (within the agent framework) move
-# randomly
-
-# a.move()
-# print(a._y, a._x)
-
-# Start of the code used to measure the time taken for the code to run.
-# start = time.time()
-
-# The following code will create a function which returns the Euclidian distance between
-# two arbitary pair of agents derived from the agent framework, using Pythagoras' theorem.
-
-# def distance_between(agents_row_a, agents_row_b):
-    # return (((agents_row_a._x - agents_row_b._x)**2) + ((agents_row_a._y - agents_row_b._y)**2))**0.5
-    
-# Create an empty list using agents which coordinates can be added to.
+# An empty list called agents is created to allow for coordinates to be added.
 
 agents = []
 
-# Create a new variable to control how many agents there will be.
+# A new variable created called "num_of_agents" will control how many agents there
+# will be.
 
 num_of_agents = 10
 
-# Create a new variable to move the agent co-ordinates an arbitary number of times.
+# Another new variable "num_of_iterations" will allow the agent coordinates
+# to move an arbitrary number of times.
 
 num_of_iterations = 100
 
-# Create a new variable to allow agents to search for close neighbours within the
-# distance attached to this variable.
+# "neighbourhood" is another variable created. This allows for agents to search
+# for close neighbours within the distance attached to this variable.
 
 neighbourhood = 20
 
-# The following code will use a for-loop defined by the num of agents to generate 
-# a list of agent coordinates, obtained from a random location within the agent
-# framework. To add the first set of coordinates to the list, the agents.append 
-# function will be used. At this point, the environment list is passed into the
-# agent's cronstructor along with the x and y variables.
+# The next section of code will connect the model to the agent framework, allowing
+# for each agent to interact with both another agent and its environment.
 
+# A for-loop defined by the num_of_agents is created to allow for a list of agent
+# coordinates to be generated.
 for i in range(num_of_agents):
-    y = int(td_ys[i].text)
-    x = int(td_xs[i].text)
+    # Both y and x variables are created using the obtained HTTP data from the web.
+    # The y and x variables are then multiplied by three to allow the coordinates to
+    # match the same extent as the environment.
+    y = int((td_ys[i].text)*3)
+    x = int((td_xs[i].text)*3)
+    # The agents.append function is used to add the first set of coordinates to the list.
+    # At this point, the environment list, along with the agents and the y and x
+    # coordinates are passed into the agent framework's constructor.
     agents.append(af_assignment.Agent(environment, agents, y, x))
 
-# Create a single agent that connects the model with the agent framework module.
+# A single agent 'a' is created to connect the model with the agent framework.
 
 a = af_assignment.Agent(environment,agents, y, x)
 
-# The following code will create a new variable which defines the figure plot and axis.
+# To test the single-agent connection, the x and y coordinates from the agent
+# framework are printed.
+
+print(a._y,a._x)
+
+# The following code tests that the coordinates from the agent framework move.
+
+print(a.move)
+
+# Start of the code used to measure the time taken for the code to run during the
+# animation.
+
+start = time.time()
+
+# The following code defines and sets two new variables, figure plot and axis.
+# These are created to help construct the model within an animation.
 
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
 
-# # Test the single agent connection by printing the x and y coordinates from the agent
-# framework.Test the link to the list of agents into each agent.
+# Random.seed is used to create a more deterministic pseudorandom sequence.
+# Here, 20 is used for the seed. This ensures that by using the same seed, the 
+# same pattern of random numbers will be generated. 
 
-print(a._y,a._x)
+random.seed(20)
 
-# Test that the co-ordinates from the agent framework move.
-
-print(a.move)
-
-    
-# To give the model behaviour, a code within a for-loop can be created to randomly 
-# move all agent co-ordinates using the move and eat methods, obtained again from the agent framework. 
-# This is achievable with the for I in range function. The for j in range function specifies 
-# the number of times the coordinates can move, which is 100. Here, the y co-ordinated will 
-# be refered to as agents[i].y and the x co-ordinate will be refered to as agents[i].x.
-# Here, x and y coordinates are edited using the move and eat methods obtained from the agent
-# framework file.
+# Carry_on is used to create a global variable that can be used inside and outside of
+# functions.
 
 carry_on = True
 
+# To allow the agents to randomly move, a function called 'def update' defined by the
+# frame number is created.
 def update(frame_number):
-     
+    # Global carry_on is called within the function, setting true, allowing the animation
+    # to continue running.
     global carry_on
+    # The following code clears the figure plot.
     fig.clear() 
-    if True:
-        
+    # As carry_on is true, random.shuffle is used to randomly shuffle the agents list.
+    if carry_on:
         random.shuffle(agents)
-        
+        # Here, a for-loop is used, using the for j in range function to specify the number
+        # of times the coordinates can move (10 times).
         for j in range(10):
+            # Within the for-loop, the number of agents is defined within for i in range.
+            # Here, move, eat and share functions obtained from the agent framework, allow
+            # the agents to interact with both other agents and the environment.
             for i in range(num_of_agents):
                 agents[i].move()
                 agents[i].eat()
                 agents[i].share_with_neighbours(neighbourhood) 
-                
-        if random.random() < 0.1:
-            carry_on = False
-            print("stopping condition")
+            
+# The following code will display the environment data, adjusted to match the area the 
+# agents are in. Here, Matplotlib.pyplot.ylim/xlim are used to set the y and x limits 
+# of the current axes. A title is also created.
         
-    matplotlib.pyplot.ylim(0, 100)
-    matplotlib.pyplot.xlim(0, 100)
+    matplotlib.pyplot.ylim(0, 300)
+    matplotlib.pyplot.xlim(0, 300)
+    matplotlib.pyplot.title("An Animation of the Movement of Sheep Around an Environment.")
     matplotlib.pyplot.imshow(environment)
-    
-    for i in range(num_of_agents):
-        matplotlib.pyplot.scatter(agents[i]._x,agents[i]._y)
 
+# The following code will plot the agent y and x coordinates on the scatter plot and
+# will be highlighted by the colour blue.
+   
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i]._x,agents[i]._y, color='blue')
+        
+# A generator function is used to yield a value that allows the function for each step 
+# of the animation to be updated. As the global variable remains true, if a is less than 10,
+# as well as less than the number of iterations, a is yielded and increased by 1.
 		
 def gen_function(b = [0]):
     a = 0
-    global carry_on #Not actually needed as we're not assigning, but clearer
+    global carry_on 
     while (a < 10) & (carry_on) :
-        yield a			# Returns control and waits next call.
-        a = a + 1
+        while (a < num_of_iterations) & (carry_on):
+            yield a			
+            a = a + 1
 
+# The following code uses a run function to allow the animation to run. Here, the 
+# stopping condition of the animation is defined by the gen_function.
 
 def run():
     animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
     canvas.draw()
 
-# The following code builds the main window ("root"); sets its title, then creates 
-# and lays out a matplotlib canvas embedded within our window and associated with fig,
-# our matplotlib figure.
+# The following code allows for the main window ("root") to be created. Here, the 
+# title is set and the matplotlib canvas is laid out within the window, displaying the
+# matplotlib figure.
 
 root = tkinter.Tk()
 root.wm_title("Model")
+root.title("Animation: Agent-Based Model")
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-# This code builds the menu bar within the main window; alongside a command function
-# that allows the model to be run.
+# The next part of the code builds the menu bar within the main window. This will use
+# a command function that allows the user to run the model.
 
 menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
@@ -207,13 +232,22 @@ model_menu = tkinter.Menu(menu_bar)
 menu_bar.add_cascade(label="Model", menu=model_menu)
 model_menu.add_command(label="Run model", command=run)
 
-# 
+# The following code defines an exiting function. Here, as the animation ends
+# and the window has been closed, the current command will be stopped.
+
+def exiting():
+    root.quit()
+    root.destroy()
+
+root.protocol('WM_DELETE_WINDOW', exiting) 
+
+# Tkinter.mainloop is used to start running the animation.
 
 tkinter.mainloop() # Wait for interactions.
 
-#animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=10)
-#animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
+# The end of the code used to measure the time taken for the code to run during the
+# animation.
 
-# Randomise the order in which agents are processed each iteration.
+end = time.time()
 
          
--- af_assignment.py
+++ af_assignment.py
@@ -6,74 +6,80 @@ Created on Sun Apr 25 11:17:21 2021
 """
 # Import relevant packages from the python library.
 # Import random provides access to random functions.
+# Import requests provides access to HTTP library, enabling HTTP requests in Python, 
+# reducing complexities behind the interface.
+# Import bs4 is a python library for pulling data out of HTML and XML files. This 
+# provides idiomatic ways of navigating, searching, and modifying the parse tree, 
+# saving the programmer time.
 
 import random
 import requests
 import bs4
 
-
-# Using an __init__ method, define an agent class using x and y variables.This 
-# code will have a parameter label called self to assign the object. Anything defined
-# to this class, belongs only to this class. The x and y variables will implement a property
-# attribute to protect the variables.
-# The move method defined within the agent class, allows the new self._x and 
-# self._y co-ordinates to move randomly.
-# If and else command statements are used to control and condition the agent 
-# co-ordinates. The random.randit function will assign random floating values 
-# between 0 and 100 to the co-ordinates. This is used to determine if the agent 
-# coordinates should increase or decrease by one based on the random number used and
-# whether this is greater or less than the value of 0.5.  
-# The torus approach is also used to solve boundary problems by using the modulus operator
-# and plotting the remainder of the division.
-# Link the agents to the enviornment to allow for interaction.Here, all agents are 
-# linked to the same environment object. This means that as one agents changes
-# the environment data, the environment changes for all agents.
-# Create a link to get the list of agents into each agent.
-
+# The following code will create a new class called agent. Within this class, an 
+# __init__ method is used to pass self, environment, agents and both the y and x 
+# coordinates into the agents constructor.
 class Agent():
     def __init__(self,environment,agents, y, x):
+        # The parameter label called self is used to assign the object. Thereby, anything 
+        # defined to this class belongs only to this class.
         self.environment = environment
         self.agents = agents
         self.store = 0
-        self._x = random.randint(0,99)
-        self._y = random.randint(0,99)
+        # The random.randit function is used to assign random floating values between 0 
+        # and 300 to the self._x and self._y coordinates, matching the extent of the
+        # environment.
+        self._x = random.randint(0,300)
+        self._y = random.randint(0,300)
+        
+# Here, if and else command statements are used to control and condition the agent
+# coordinates. These again, use random.randit to assign random floating values to
+# the same extent as the environment.
 
         if (x == None):
-            self._x = random.randint(0,100)
+            self._x = random.randint(0,300)
         else:
             self._x = x
             
         if (y == None):
-            self._y = random.randint(0,100)
+            self._y = random.randint(0,300)
         else:
             self._y = y
+                        
+# The following code will use a move function defined by self within the agent class.
+# This will allow the new self._x and self._y coordinates to move randomly. The value of
+# the self._x and self._y coordinates should increase or decrease by one based on the random
+# number generated and whether this is greater or less than the value of 0.5. A torus approach
+# is also used to solve boundary problems by using the modulus operator and plotting the 
+# remainder of the division.
         
     def move(self):
         if random.random() < 0.5:
-            self._x = (self._x + 1) % 100
+            self._x = (self._x + 1) % 300
         else:
-            self._x = (self._x - 1) % 100
+            self._x = (self._x - 1) % 300
     
         if random.random() < 0.5:
-            self._y = (self._y + 1) % 100
+            self._y = (self._y + 1) % 300
         else:
-            self._y = (self._y - 1) % 100
+            self._y = (self._y - 1) % 300
             
-# The following code will use an eat method within the agent class. This allows 
-# for the environment data to be altered. If self._x and self._y are greater than
-# 10, the environment will minus 10 and add it to the store.
-       
+# The following code defines an eat function within the agent class, associated with self.
+# This links all agents to the same environment object. Thereby, as one agent changes the
+# environment data, the environment changes for all agents. For instance, If self._x and 
+# self._y are greater than 10, the environment will minus 10 and add it to the store.
+
     def eat(self): 
-        if self.environment[self._y][self._x] > 10:
+         if self.environment[self._y][self._x] > 10:
             self.environment[self._y][self._x] -= 10
             self.store += 10
 
-# The following code will make a sharing neighbour method within the agent class.
-# This will allow the agent to search for close neighbours and share resources with
-# them. This method will calculate the distance to each of the other agents, and if
-# they fall within neighbourhood distance, it should set it, and its neighbours'
-# stores equal to the average of their two stores.
-          
+# The code below defines a share_with_neighbours function within the agent class, associated 
+# with self and neighbourhood. This will allow the agents to search for close neighbours and 
+# share resources with them. Here, the distance to each of the other agents will be calculated,
+# and if they fall within neighbourhood distance, it should set it and its neighbours' stores
+# equal to the average of their two stores.
+        
     def share_with_neighbours(self, neighbourhood):
         for agent in self.agents:
             dist = self.distance_between(agent)
@@ -82,25 +88,15 @@ class Agent():
             average = sum /2
             self.store = average
             agent.store = average
-        
-      
-      # print("sharing " + str(dist) + " " + str(average))
             
-# The following code will create a function which returns the Euclidian distance between
-# each of the agents using Pythagoras' theorem.
+# The following code will define a function within the agent class, which returns the 
+# Euclidian distance between each of the agents using Pythagoras' theorem.
 
     def distance_between(self,agent):
         return (((self._x -agent._x)**2) + ((self._y - agent._y)**2))**0.5
-                  
-# Set the property x and y.
-            
-    #def x(self):
-        #return self._x
         
-    #def y(self):
-        #return self._y
-        
-# Use the get function to obtain the atrribute value for the x and y co-ordinates.
+# Using a get function defined by self within the agent class allows for the attribute
+# values for the self._x and self._y coordinates to be obtained.
       
     def getx(self):
         return self._x
@@ -108,7 +104,8 @@ class Agent():
     def gety(self):
         return self._y
     
-# Use the self function to set the attribute value for the x and y co-ordinates.
+# Here, a set function is defined within the agent class, associated with self. This
+# will set the attribute values for the self._x and self._y coordinates.
 
     def setx(self, value):
         self._x = value
